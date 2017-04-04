4cb523bda9fe3287403067824cb019c39e682512
do7be <bomsap700@gmail.com>
2017-04-04 20:38:01 +0900

    html2canvasをやめた

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
+import FileSaver from 'file-saver'
+
 document.getElementById('cap').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      mode: 'capture'
-    }, function(msg) {
-      // console.log("result message:", msg);
-    })
+  chrome.tabs.captureVisibleTab(null, { format: "jpeg" }, function(hoge) {
+    const block = hoge.split(";")
+    const mimeType = block[0].split(":")[1] // In this case "image/jpg"
+    const realData = block[1].split(",")[1] // For example:  iVBORw0KGgouqw23....
+    const canvasBlob = b64toBlob(realData, mimeType)
+
+    FileSaver.saveAs(canvasBlob, "screenshot.jpg")
   })
 })
+
+function b64toBlob(b64Data, contentType, sliceSize) {
+  contentType = contentType || ''
+  sliceSize = sliceSize || 512
+
+  const byteCharacters = atob(b64Data)
+  let byteArrays = []
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize)
+    let byteNumbers = new Array(slice.length)
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i)
+    }
+    const byteArray = new Uint8Array(byteNumbers)
+
+    byteArrays.push(byteArray)
+  }
+
+  const blob = new Blob(byteArrays, {
+    type: contentType
+  })
+  return blob
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
   context: path.resolve(__dirname, './src'),
   entry: {
     index: './index.js',
-    content_script: './content_script.js',
   },
   output: {
     path: path.resolve(__dirname, './dist'),
